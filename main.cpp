4fd3befe5748dd8fc705a6bbf97f529fcb4217fd
Кутараев Артём Алексеевич ИП-515 <rezlov@MacBook-Pro-Artem.local>
2025-10-02 12:39:35 +0700

    Исправлено приветствие
    # Conflicts:
    #	main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #define SIZE 1024
+
 int main(int argc, char** argv) {
-char* str = (char*)malloc(sizeof(char) * SIZE);
-printf("Введите своё имя:\n");
-str = gets(str);
-printf("Привет, %s", str);
-return 0;
+    char* str = (char*)malloc(sizeof(char) * SIZE);
+    printf("Введите своё имя:\n");
+    str = gets(str);
+    puts("Привет");
+    puts(str);
+    return 0;
 }
